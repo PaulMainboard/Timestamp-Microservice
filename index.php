450fe833d236a5abadd70ef278146eae489cbd44
PaulMainboard <paulwilliams669@gmail.com>
2017-11-27 18:27:35 -0500

    Have the program decide what to do if the date parameter is or is not entered. Plus make the parameter case insensitive.

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 
-if ($_GET) { // If parameters are entered in the URL.
-    $date = $_GET['date'];
+// Make the date parameter case insensitive.
+$dateParameter = array_change_key_case($_GET, CASE_LOWER); 
+
+if (array_key_exists('date', $dateParameter) &&
+   trim($dateParameter['date']) != "") { // If date parameter are entered in the URL.
+    $date = $dateParameter['date'];
 } else { // If the URL do not have any parameters.
     $date = "null";
 }
