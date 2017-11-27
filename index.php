541b6d31a5c68ffd6e893beb5a6707c29ecc2ae9
PaulMainboard <paulwilliams669@gmail.com>
2017-11-27 17:57:32 -0500

    Get date parameter from the URL.

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
 
+if ($_GET) { // If parameters are entered in the URL.
+    $date = $_GET['date'];
+} else { // If the URL do not have any parameters.
+    $date = "null";
+}
+
+// Print date ** FOR TESTING PURPOSES **
+echo $date;
