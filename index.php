da17a1ee0fc600ba894275a4e60c648b12be4405
PaulMainboard <paulwilliams669@gmail.com>
2017-11-28 18:25:35 -0500

    Add the format_constant_datetime_input_month method to the TimeFormatConvertor Class

--- index.php
+++ index.php
@@ -8,7 +8,8 @@ $dateParameter = array_change_key_case($_GET, CASE_LOWER);
 if (array_key_exists('date', $dateParameter) &&
    trim($dateParameter['date']) != "") { // If date parameter are entered in the URL.
     $date = $dateParameter['date'];
-    // new DateTime($dateParameter['date']); // TESTING IN PROGRESS ** Change a string into a DateTime object **
+    $testDate =  new DateTime(/*$dateParameter['date']*/ "9-12-2017"); // TESTING IN PROGRESS ** Change a string into a DateTime object **
+    /* DateTime formats:  */ 
     $timestamp = 0;
 //    if (is_string($date)) { // TESTING IN PROGRESS ** Get Timestamp of a date that is a string **
 //        $timestamp = strtotime($date);
@@ -24,3 +25,5 @@ if (array_key_exists('date', $dateParameter) &&
 echo $date . "<br>" . $timestamp;
 echo "<br>" . var_dump(strtotime(str_replace(" ", "-",$date))) . "<br>";
 echo TimeFormatConvertor::str_month_to_str_num("november");
+echo "<br>" . $testDate->format("m d y");
+echo "<br>" . TimeFormatConvertor::format_constant_datetime_input_month("9-12-2017");
