ee41133f49c2cd39e6d7b815a2d1e057f5fab784
PaulMainboard <paulwilliams669@gmail.com>
2017-11-29 02:31:16 -0500

    Clean up code within the index.php file.

--- index.php
+++ index.php
@@ -8,38 +8,38 @@ include "classes/TimeFormatConvertorClass.php";
 //} else { //If parameter is a date.
 //    echo "date";
 //}
-//
-////Conditions for parameters entered in dashed format.
-//if (preg_match("/^[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}$/",$_GET['date'])) {
-//    echo "<br>Format: YYYY-MM-DD <br>";
-//} else if (preg_match("/^[0-9]{1,2}-[0-9]{1,2}-[0-9]{4}$/",$_GET['date'])) {
-//    echo "<br>Format: MM-DD-YYYY <br>";
-//}
 
 // Make the date parameter case insensitive.
 $dateParameter = array_change_key_case($_GET, CASE_LOWER); 
 
-if (array_key_exists('date', $dateParameter) &&
-   trim($dateParameter['date']) != "") { // If date parameter are entered in the URL.
-     
-//    if (!preg_match("/^[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}$/",$dateParameter['date']) && !preg_match("/^[0-9]{1,2}-[0-9]{1,2}-[0-9]{4}$/",$dateParameter['date'])) {
-//            
-//        $dateParameter = date("m d y",strtotime(trim($dateParameter['date'])));
-//        echo var_dump(strtotime($dateParameter));   
-//    }
+if (  // If date parameter are entered in the URL.
+    array_key_exists('date', $dateParameter) &&
+    trim($dateParameter['date']) != ""
+   ) {
     
     /* If the date parameter is a string */
 
-    // If the date format is numerical and has dashes in it. (example: YYYY-M-D or M-D-YYYY)
+
     /* ***NOTE: Need to make a method to handle date format M-D-YY*** */
-    if (preg_match("/^[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}$/",$dateParameter['date']) || preg_match("/^[0-9]{1,2}-[0-9]{1,2}-[0-9]{4}$/",$dateParameter['date'])) { 
+    
+    if ( // If the date format is numerical and has dashes in it. (example: YYYY-M-D or M-D-YYYY)
+        preg_match("/^[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}$/",$dateParameter['date']) || 
+        preg_match("/^[0-9]{1,2}-[0-9]{1,2}-[0-9]{4}$/",$dateParameter['date'])
+    ) { 
+        
         $dateParameter = $dateParameter['date'];
+        
     } else { // If the date format is in any other format that does not include dashes.
+        
             $dateParameter = date("y-m-d",strtotime(trim($dateParameter['date'])));     
+        
     }
     
     // If the date format is M-D-YYYY, bring the year to the front of the string (ex: YYYY-M-D).
-    if (preg_match("/^[0-9]{1,2}-[0-9]{1,2}-[0-9]{4}$/",$dateParameter)) { 
+    if (
+        preg_match(
+            "/^[0-9]{1,2}-[0-9]{1,2}-[0-9]{4}$/",$dateParameter
+        )) { 
             $dateParameter = TimeFormatConvertor::format_constant_datetime_input_month($dateParameter);   
     }
     
@@ -51,6 +51,7 @@ if (array_key_exists('date', $dateParameter) &&
 //    if (is_string($date)) { // TESTING IN PROGRESS ** Get Timestamp of a date that is a string **
 //        $timestamp = strtotime($date);
 //    }
+    
     $date= $date->format("m-d-Y"); // Set the date in dashed format. (example: 11-24-2018)
     
 } else { // If the URL do not have any parameters.
