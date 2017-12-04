684ae7cf528fff7db71b9a8030e957b1ccd7e64c
PaulMainboard <paulwilliams669@gmail.com>
2017-12-04 02:07:40 -0500

    Started working on a frontend interface.

---
+++ pages/page.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>Timestamp-Microservice</title>
+        <link rel="stylesheet" href="../css/style.css">
+    </head>
+    <body>
+        <div id="container">
+            <header>
+                <h1>Timestamp Microservice</h1>
+                <div id="directions"><a href="#">Click Here For Directions</a></div>
+            </header>
+            <div id="content">
+                <div id="inputArea">
+                    <input type="text">
+                    <button>Enter</button>
+                </div>
+                <div id="dates"></div>
+            </div>
+        </div>
+        
+        
+    </body>
+</html>
